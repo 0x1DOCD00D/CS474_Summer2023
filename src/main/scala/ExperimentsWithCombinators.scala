b4a7f83311774d6e52a2563fc285490b12e66b86
Mark Grechanik <drmark@uic.edu>
2023-06-01 13:47:07 -0500

    map/pure on 6/1

---
+++ src/main/scala/ExperimentsWithCombinators.scala
@@ -0,0 +1,21 @@
+/*
+ *
+ *  Copyright (c) 2021. Mark Grechanik and Lone Star Consulting, Inc. All rights reserved.
+ *   
+ *   Unless required by applicable law or agreed to in writing, software distributed under
+ *   the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *   either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+ *  
+ */
+
+object ExperimentsWithCombinators:
+  case class Wrapper[T](v: T)
+  def pure[T](v:T): Wrapper[T] =
+    Wrapper(v)
+
+  def map[T, S](coll: Wrapper[T])(f: T=>S): Wrapper[S] = pure(f(coll.v))
+
+  @main def runMain_ExperimentsWithCombinators(): Unit =
+    val res = "Luis".length
+    val coll = map(pure("Luis"))((s:String)=>s.length)//List("Luis")
+    println(coll)
