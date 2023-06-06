9884d6429af033c6694abe32f0e8f67616c0f159
Mark Grechanik <drmark@uic.edu>
2023-06-06 18:33:48 -0500

    not a string param on 6/6

---
+++ src/main/scala/BlowItAllUp.scala
@@ -0,0 +1,23 @@
+/*
+ *
+ *  Copyright (c) 2021. Mark Grechanik and Lone Star Consulting, Inc. All rights reserved.
+ *   
+ *   Unless required by applicable law or agreed to in writing, software distributed under
+ *   the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *   either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+ *  
+ */
+
+object BlowItAllUp:
+  val listOfFunctions = List.fill(1_000_000)((i:Int)=>i+1)
+  def composeFunctions(lof:List[Int=>Int]): Int=>Int =
+    (i:Int) => lof match
+      case Nil => 0
+      case hd::tl =>  composeFunctions(tl)(hd(0))
+//      case hd::tl => hd andThen(composeFunctions(tl))
+  end composeFunctions
+
+  @main def runMain_BlowItAllUp(): Unit =
+//    println(listOfFunctions)
+//    println(composeFunctions(listOfFunctions)(0))
+    println(listOfFunctions.foldLeft(0)((acc, f)=> f(acc)))
---
+++ src/main/scala/NotStringParam.scala
@@ -0,0 +1,22 @@
+/*
+ *
+ *  Copyright (c) 2021. Mark Grechanik and Lone Star Consulting, Inc. All rights reserved.
+ *   
+ *   Unless required by applicable law or agreed to in writing, software distributed under
+ *   the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *   either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+ *  
+ */
+
+object NotStringParam:
+  sealed trait NotAString[X]
+
+  object NotAString {
+    implicit def StringIsNotValClass[X <: String]: NotAString[X] = new NotAString[X] {}
+  }
+
+  def anyTypeButString[X: NotAString](z: X) = z
+
+  @main def runMain_NotStringParam(): Unit =
+    println(anyTypeButString("howdy!"))
+
