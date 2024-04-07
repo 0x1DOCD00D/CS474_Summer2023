1689df50d43797dd43aa3371c03957cc39ec19a1
0x1DOCD00D <markgrechanik@gmail.com>
2024-04-07 12:39:45 -0500

    Added a template object for scala experiments

---
+++ src/main/scala/Class474_Summer_2024.scala
@@ -0,0 +1,5 @@
+package cs.uic
+
+object Class474_Summer_2024:
+  @main def runClass474_Summer_2024(parm: String*) =
+    println("This is the main template for trying various programming concepts")
